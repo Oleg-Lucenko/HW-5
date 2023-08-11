4f847bfb2a2d677407102910ebfd3df9977f2989
Oleg-Lucenko <lucenko5577@gmail.com>
2023-08-11 23:16:15 +0300

    HW-5, Lucenko

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import NewTask from './components/New-task';
+import Tasks from './components/Tasks';
+import Edit from './components/Edit';
 import './App.css';
 
+
+
+
 function App() {
+  const [tasks, setNewTask] = useState([]);
+  const [taskForEdit, setTaskForEdit] = useState({});
+  const [editFormStatus, setEditFormStatus] = useState(false);
+
+  function addTask(newItem, inputValue) {
+    if (inputValue) {
+    setNewTask([...tasks, newItem]);
+    };
+  };
+
+  function removeTask(taskID) {
+    setNewTask(tasks.filter(task => task.id != taskID));
+  };
+
+  function taskStatus(taskID) {
+    setNewTask(tasks.map(task => task.id === taskID ? { ...task, taskStatus: !task.taskStatus } : {...task} ));
+  };
+
+  function taskForEditPrepare(taskID) {
+    setEditFormStatus(true);
+    const [taskForEdit] = tasks.filter(task => task.id === taskID);
+    setTaskForEdit(taskForEdit);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {editFormStatus && (
+      <Edit taskForEditPrepare={taskForEditPrepare} taskForEdit={taskForEdit} tasks={tasks} setNewTask={setNewTask} setEditFormStatus={setEditFormStatus}/>
+      )}
+        <NewTask tasks={tasks} addTask={addTask}/>
+        <Tasks tasks={tasks} removeTask={removeTask} taskStatus={taskStatus} taskForEditPrepare={taskForEditPrepare}/>
     </div>
   );
 }
